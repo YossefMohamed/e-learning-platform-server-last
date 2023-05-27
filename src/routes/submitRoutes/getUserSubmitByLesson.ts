223f6e48de718a240e6b67e8944afc50f8f6846b
YossefAbdelMohsen <yossefmohamed112233@gmail.com>
2023-05-27 22:45:24 +0300

    finish submit route

--- src/routes/submitRoutes/getUserSubmitByLesson.ts
+++ src/routes/submitRoutes/getUserSubmitByLesson.ts
@@ -3,6 +3,6 @@ import { getUserSubmitByLesson } from "../../controllers/submitControllers";
 
 const router = Router();
 
-router.get("/:lesson/user", getUserSubmitByLesson);
+router.get("/users/:lesson", getUserSubmitByLesson);
 
 export { router as getUserSubmitByLessonRouter };
