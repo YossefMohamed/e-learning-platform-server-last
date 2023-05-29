cd9f846280a8f0a10872d2246368e9f162d38a92
YossefAbdelMohsen <yossefmohamed112233@gmail.com>
2023-05-30 00:03:36 +0300

    add request validtors for submits routes

--- src/routes/submitRoutes/reviewSubmit.ts
+++ src/routes/submitRoutes/reviewSubmit.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { ReviewSubmit } from "../../controllers/submitControllers";
+import { getSubmitValidators } from "../../services/submitsValidators/getSubmitValidators";
+import { validateRequest } from "../../middlewares/validate-request";
 
 const router = Router();
 
 router.get(
   "/reviews/:lesson",
+  getSubmitValidators,
 
+  validateRequest,
   ReviewSubmit
 );
 
